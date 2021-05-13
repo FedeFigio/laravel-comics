c03bc8c5344c3d6872cd5f3960de1e76f33598ef
Federico <federicofigini90@gmail.com>
2021-05-13 12:58:00 +0200

    wp risoluzione 1200 px

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('main')
+    <section id="home">
+        <div class="container flex wrap between">
+            <div class="current">
+                current series
+            </div>
+            @foreach ($comics as $index => $comic)
+            <a href="" class="card">
+                <div class="thumb">
+                    <img src="{{$comic['thumb']}}" alt="">
+                </div>
+                <div class="title">
+                    {{$comic['series']}}
+                </div>
+            </a>
+            @endforeach
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<header class="flex column">
+    <div class="flex">
+        <div class="container"></div>
+    </div>
+    <nav class="flex ">
+        <div class="container flex between a-center ">
+            <div class="logo">
+                <img src="images/dc-logo.png" alt="">
+            </div>
+            <ul class="flex ">
+                <li>characters</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+            <input type="search" placeholder="Search">
+        </div>
+    </nav>
+    <div class="jumbo">
+
+    </div>
+</header>
